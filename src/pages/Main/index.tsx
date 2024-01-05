4a8619c63b6f8199007b13a780249f065ae216cc
AlexeyKiselev824 <smoke824@yandex.ru>
2024-01-05 20:21:55 +0300

    initial scss base style

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -2,16 +2,17 @@ import { FC } from 'react';
 import { Outlet } from 'react-router-dom';
 import Footer from 'src/containers/Footer';
 import Header from 'src/containers/Header';
+import 'src/index.scss';
 
 const Main: FC = () => {
     return (
-        <>
+        <div className="App">
             <Header />
             <main className="_main">
                 <Outlet />
             </main>
             <Footer />
-        </>
+        </div>
     );
 };
 
